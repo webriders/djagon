6dd5de98273d6ee8bd3bc501622d10af7f5a51e2
Alexander Gorodetsky <alexander.gorodetsky@webriders.com.ua>
2013-09-28 17:28:45 +0300

    cards generation

---
+++ site/source/game/cards.py
@@ -0,0 +1,26 @@
+colors = ['red', 'green', 'blue', 'yellow']
+values = [str(x) for x in range(1, 10)] * 2 + ['reverse', 'skip', 'draw-two'] * 2 + ['0']
+
+wild_cards = [{
+                  'color': 'black',
+                  'value': 'wild',
+              }] * 4
+
+draw_four_cards = [{
+                       'color': 'black',
+                       'value': 'draw-four'
+                   }] * 4
+
+
+def generate_cards():
+    cards = []
+    for col in colors:
+        for val in values:
+            cards.append({
+                'color': col,
+                'value': val,
+            })
+    return cards + wild_cards + draw_four_cards
+
+
+cards = generate_cards()
