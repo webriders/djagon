df55aa074d45b7415390473cc766b5186988f68d
Rostyslav Bryzgunov <kottenator@gmail.com>
2013-09-30 00:28:37 +0300

    Merged

--- site/source/game/static/game/js/game.js
+++ site/source/game/static/game/js/game.js
@@ -270,10 +270,6 @@ djagon.game.Game.prototype = {
         this.updateYourCurrentCards(d3.select(this.container[0]).selectAll('.card.your'));
     },
 
-    makeTurn: function(card_id) {
-         this.socket.emit("make_turn", this.gameId, card_id);
-    },
-
     createYourNewCards: function(cards) {
         console.log('createYourNewCards', cards[0].length);
 
@@ -281,6 +277,9 @@ djagon.game.Game.prototype = {
 
         cards.append('div')
             .classed('card your', true)
+            .on("dblclick", function(d) {
+                self.makeTurn(d.id);
+            })
             .append('img')
             .attr('src', function(d) {
                 var color = d.color,
@@ -402,5 +401,9 @@ djagon.game.Game.prototype = {
             this.socket.emit("start_confirm", this.gameId);
             console.log('start_confirm', 'sent');
         }
+    },
+
+    makeTurn: function(card_id) {
+         this.socket.emit("make_turn", this.gameId, card_id);
     }
 };
