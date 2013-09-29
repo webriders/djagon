74100b69cc8c659b27f2a11ba4c6c55daa72afbe
pavlo <skliarpawlo@rambler.ru>
2013-09-29 19:03:28 +0300

    start_game, start_confirm

--- site/source/game/sockets.py
+++ site/source/game/sockets.py
@@ -13,3 +13,38 @@ class GameNamespace(BaseNamespace):
 
         game_mechanics = GameMechanics(game, self.socket, self.session, self.ns_name)
         game_mechanics.on_join_game()
+
+    def on_start_confirm(self, game_id):
+        game = GameTable.get_game(game_id)
+        if game is None:
+            return
+
+        player = game.players[self.session['player_id']]
+        player.lamp = True
+        game.save()
+
+        game_mechanics = GameMechanics(game, self.socket, self.session, self.ns_name)
+
+        are_all_players_confirmed = True
+        for x in game.players:
+            if game.players[x].lamp == False:
+                are_all_players_confirmed = False
+
+        if not are_all_players_confirmed:
+            game_mechanics._send_initial_game_state()
+        else:
+            game_mechanics._send_game_start()
+
+    def on_start_unconfirm(self, game_id):
+        game = GameTable.get_game(game_id)
+        if game is None:
+            return
+
+        player = game.players[self.session['player_id']]
+        player.lamp = False
+        game.save()
+
+        game_mechanics = GameMechanics(game, self.socket, self.session, self.ns_name)
+
+        game_mechanics._send_initial_game_state()
+
